dfb6f460d746806ad1ff811d7cfe3601fcc03317
Andrew Drechsel <roydrechsel@gmail.com>
2017-03-07 20:55:37 -0700

    Model, and nearly nothing on the storyboard

--- SongRight/FirstTableViewController.swift
+++ SongRight/FirstTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ SongRight/Recordings.swift
@@ -0,0 +1,30 @@
+//
+//  Recordings.swift
+//  SongRight
+//
+//  Created by Andrew Drechsel on 3/7/17.
+//  Copyright © 2017 Andrew Drechsel. All rights reserved.
+//
+
+import Foundation
+
+class Recordings: Equatable {
+    
+    var title: String
+    var timestamp: Date
+    var length: Double
+    
+    init(title: String, timestamp: Date = Date(), length: Double) {
+        
+        self.title = title
+        self.timestamp = timestamp
+        self.length = length
+    }
+}
+
+
+func ==(lhs: Recordings, rhs: Recordings) -> Bool {
+    
+    return lhs.title == rhs.title && lhs.timestamp == rhs.timestamp && lhs.length == rhs.length
+    
+}
